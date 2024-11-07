4dd99cc6b334b464adcbd851fce8fa5b15fb5abb
jay-so <merry9504@naver.com>
2024-11-07 13:17:50 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/org/indoles/memberserviceserver/MemberServiceServerApplication.java
@@ -0,0 +1,13 @@
+package org.indoles.memberserviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServiceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberServiceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/indoles/memberserviceserver/MemberServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.indoles.memberserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
