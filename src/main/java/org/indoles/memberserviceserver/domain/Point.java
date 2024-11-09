5ec569213d98f0f789b3e6a2af82e0b360413a18
jay-so <merry9504@naver.com>
2024-11-09 23:08:14 +0900

    style: Point 클래스 사용 validation 변수명 변경

--- src/main/java/org/indoles/memberserviceserver/domain/Point.java
+++ src/main/java/org/indoles/memberserviceserver/domain/Point.java
@@ -2,29 +2,27 @@ package org.indoles.memberserviceserver.domain;
 
 import lombok.Getter;
 import org.indoles.memberserviceserver.domain.validate.ValidatePoint;
-import org.indoles.memberserviceserver.entity.exception.MemberException;
-import org.indoles.memberserviceserver.entity.exception.MemberExceptionCode;
 
 @Getter
 public class Point {
 
     private Long amount;
-    private final ValidatePoint validator = new ValidatePoint();
+    private final ValidatePoint validatePoint = new ValidatePoint();
 
     public Point(Long amount) {
-        validator.validatePositiveAmount(amount);
+        validatePoint.validatePositiveAmount(amount);
         this.amount = amount;
     }
 
     public void minus(Long minusAmount) {
-        validator.validatePositiveAmount(minusAmount);
-        validator.validateMinusPoint(amount, minusAmount);
+        validatePoint.validatePositiveAmount(minusAmount);
+        validatePoint.validateMinusPoint(amount, minusAmount);
         amount -= minusAmount;
     }
 
     public void plus(Long price) {
-        validator.validatePositiveAmount(price);
-        validator.validatePlusPoint(amount, price);
+        validatePoint.validatePositiveAmount(price);
+        validatePoint.validatePlusPoint(amount, price);
         amount += price;
     }
 }
