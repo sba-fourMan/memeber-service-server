e69f6147436ff4c221ba3c30a0ba1ba1553f8c8f
jay-so <merry9504@naver.com>
2024-11-16 22:15:55 +0900

    refactor: Repository 관련 로직 리팩토링

--- src/main/java/org/indoles/memberserviceserver/core/infra/MemberRepository.java
+++ src/main/java/org/indoles/memberserviceserver/core/infra/MemberRepository.java
@@ -1,8 +1,6 @@
-package org.indoles.memberserviceserver.core.repository;
+package org.indoles.memberserviceserver.core.infra;
 
 import org.indoles.memberserviceserver.core.domain.Member;
-import org.indoles.memberserviceserver.core.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
