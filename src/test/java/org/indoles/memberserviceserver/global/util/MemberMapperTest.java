07109b3c380da19cb0fdfc18582e9f65c1f9b3ea
jay-so <merry9504@naver.com>
2024-11-16 18:50:49 +0900

    style: MemberMapperTest 테스트명 수정

--- src/test/java/org/indoles/memberserviceserver/global/util/MemberMapperTest.java
+++ src/test/java/org/indoles/memberserviceserver/global/util/MemberMapperTest.java
@@ -4,7 +4,6 @@ import org.indoles.memberserviceserver.core.domain.Member;
 import org.indoles.memberserviceserver.core.domain.Point;
 import org.indoles.memberserviceserver.core.entity.MemberEntity;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,56 +11,54 @@ import static org.indoles.memberserviceserver.core.domain.enums.Role.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class MemberMapperTest {
-    @Nested
-    class MemberMapper_Success {
-        @Test
-        @DisplayName("영속성 엔티티를 도메인 엔티티로 변환하면 정보가 동일하다")
-        void transferMemberEntityToMember() {
-            // given
-            MemberEntity entity = MemberEntity.builder()
-                    .id(1L)
-                    .signInId("helloworld")
-                    .password("password1234")
-                    .role(BUYER)
-                    .point(12345L)
-                    .build();
 
-            // when
-            Member member = Mapper.convertToMember(entity);
+    @Test
+    @DisplayName("엔티티를 도메인으로 변환하면 정보가 동일하다")
+    void transferMemberEntityToMember() {
+        // given
+        MemberEntity entity = MemberEntity.builder()
+                .id(1L)
+                .signInId("testId")
+                .password("password1234")
+                .role(BUYER)
+                .point(12345L)
+                .build();
 
-            //then
-            assertAll(
-                    () -> assertThat(member.getId()).isEqualTo(1L),
-                    () -> assertThat(member.getSignInId()).isEqualTo("helloworld"),
-                    () -> assertThat(member.getPassword()).isEqualTo("password1234"),
-                    () -> assertThat(member.getRole()).isEqualTo(BUYER),
-                    () -> assertThat(member.getPoint().getAmount()).isEqualTo(12345L)
-            );
-        }
+        // when
+        Member member = Mapper.convertToMember(entity);
 
-        @Test
-        @DisplayName("도메인 엔티티를 영속성 엔티티로 변환하면 정보가 동일하다")
-        void transferMemberToMemberEntity() {
-            // given
-            Member member = Member.builder()
-                    .id(1L)
-                    .signInId("helloworld")
-                    .password("password1234")
-                    .role(BUYER)
-                    .point(new Point(12345))
-                    .build();
+        //then
+        assertAll(
+                () -> assertThat(member.getId()).isEqualTo(1L),
+                () -> assertThat(member.getSignInId()).isEqualTo("testId"),
+                () -> assertThat(member.getPassword()).isEqualTo("password1234"),
+                () -> assertThat(member.getRole()).isEqualTo(BUYER),
+                () -> assertThat(member.getPoint().getAmount()).isEqualTo(12345L)
+        );
+    }
 
-            // when
-            MemberEntity entity = Mapper.convertToMemberEntity(member);
+    @Test
+    @DisplayName("도메인을 엔티티로 변환하면 정보가 동일하다")
+    void transferMemberToMemberEntity() {
+        // given
+        Member member = Member.builder()
+                .id(1L)
+                .signInId("testId")
+                .password("password1234")
+                .role(BUYER)
+                .point(new Point(12345))
+                .build();
 
-            // then
-            assertAll(
-                    () -> assertThat(entity.getId()).isEqualTo(1L),
-                    () -> assertThat(entity.getSignInId()).isEqualTo("helloworld"),
-                    () -> assertThat(entity.getPassword()).isEqualTo("password1234"),
-                    () -> assertThat(entity.getRole()).isEqualTo(BUYER),
-                    () -> assertThat(entity.getPoint()).isEqualTo(12345L)
-            );
-        }
+        // when
+        MemberEntity entity = Mapper.convertToMemberEntity(member);
+
+        // then
+        assertAll(
+                () -> assertThat(entity.getId()).isEqualTo(1L),
+                () -> assertThat(entity.getSignInId()).isEqualTo("testId"),
+                () -> assertThat(entity.getPassword()).isEqualTo("password1234"),
+                () -> assertThat(entity.getRole()).isEqualTo(BUYER),
+                () -> assertThat(entity.getPoint()).isEqualTo(12345L)
+        );
     }
 }
